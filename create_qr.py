a1dd7cd1b48fb9497279785c9ec9a90bf48978f9
wafwoof <kazmcquaid@gmail.com>
2023-03-01 21:20:54 -0800

    fixed qr in pic directory

--- create_qr.py
+++ create_qr.py
@@ -6,5 +6,5 @@ img = qrcode.make('http://www.google.com/', image_factory=qrcode.image.pil.PilIm
 # scale the image to have a maximum size of 128x128 pixels
 img = img.resize((128, 128), resample=Image.LANCZOS)
 
-with open('qr.png', 'wb') as qr:
+with open('pic/qr.png', 'wb') as qr:
     img.save(qr)
