eb7a58d835667d0aa27aed8745c6bec7fe3fa19a
wafwoof <kazmcquaid@gmail.com>
2023-03-02 20:40:51 -0800

    attempt crop qr code

--- create_qr.py
+++ create_qr.py
@@ -7,6 +7,8 @@ def create_qr(url):
     img = qrcode.make(f"ssh://{url}", image_factory=qrcode.image.pil.PilImage)
     # scale the image to have a maximum size of 128x128 pixels
     img = img.resize((128, 128), resample=Image.LANCZOS)
+    # remove the white border
+    img = img.crop((4, 4, 124, 124))
 
     with open('pic/qr.png', 'wb') as qr:
         img.save(qr)
