22f6e9189b2ebfb7189616df3eb8f59a4c4d3582
wafwoof <kazmcquaid@gmail.com>
2023-03-02 21:19:43 -0800

    more formatting...

--- main.py
+++ main.py
@@ -69,9 +69,9 @@ try:
     # create and draw qr code (left side of display)
     create_qr(ip)
     qr = Image.open(os.path.join(pic_dir, 'qr.png'))
-    bw_image_buffer.paste(qr, (32, 0))
+    bw_image_buffer.paste(qr, (16, 0))
     #draw.text((8, 0), "Storlabs ©", font=top_font, fill=0, align='left')
-    draw.text((0, 96), ip, font=top_font, fill=0, align='left')
+    draw.text((0, 96), ip, font=info_font, fill=0, align='left')
 
 
     # write buffer to display
