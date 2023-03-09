09d302fc14c488d76d6e1ce12af0b8626f570e0c
wafwoof <kazmcquaid@gmail.com>
2023-03-08 22:44:09 -0800

    fixing overlap

--- main.py
+++ main.py
@@ -79,9 +79,9 @@ def screen3():
     draw.text((3, 0), f"File Browser", font=header_font, fill=0, align='left')
     draw.text((3, 16), f"Viewing: {dirname}", font=header_font, fill=0, align='left')
     draw.text((3, 32), f"{cursor1} Change Directory", font=info_font, fill=0, align='left')
-    draw.text((3, 32), f"{cursor0}{len(listing) } Files listed", font=info_font, fill=0, align='left')
-    draw.text((3, 48), f"{cursor0}Refresh", font=info_font, fill=0, align='left')
-    draw.text((3, 64), f"{cursor0}Quit to Menu", font=info_font, fill=0, align='left')
+    draw.text((3, 48), f"{cursor0}{len(listing) } Files listed", font=info_font, fill=0, align='left')
+    draw.text((3, 64), f"{cursor0}Refresh", font=info_font, fill=0, align='left')
+    draw.text((3, 80), f"{cursor0}Quit to Menu", font=info_font, fill=0, align='left')
 
 
     # draw a vertical line between file list and file info
